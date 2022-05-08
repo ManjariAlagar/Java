d0348270b52769a40d1fb2e58f27e7afd2b4ac53
ManjariAlagar <56513108+ManjariAlagar@users.noreply.github.com>
2022-05-09 00:07:29 +0530

    Add files via upload

---
+++ Arithmeticoperators.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Arithmeticoperators{
+public static void main(String args[])
+{
+	Scanner sc=new Scanner(System.in);
+	int a,b;
+	a=sc.nextInt();
+	b=sc.nextInt();
+	double c;
+	c=(double)a+b;
+	System.out.println(c);
+	a++;
+	++a;
+	System.out.println(a);
+	
+	
+	
+}
+}
