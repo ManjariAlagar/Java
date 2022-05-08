df7e70ab1a44877fa8970a7035c04675a2ac8570
ManjariAlagar <56513108+ManjariAlagar@users.noreply.github.com>
2022-05-08 14:24:55 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Datatypes{
+	public static void main(String args[])
+	{
+		int val1=10;
+		int val2=123_456;
+		long lng=7l;
+		char val3='A';
+		int bin=0b01;
+		int oct=010;
+		int hex=0XA;
+		float flt=5;
+		System.out.println(val1);
+		System.out.println(val2);
+		System.out.println(lng);
+		System.out.println(val3);
+		System.out.println(bin);
+		System.out.println(oct);
+		System.out.println(hex);
+		System.out.println(flt);
+		
+	}
+}
---
+++ Variables.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Variables{
+	public static void main(String args[])
+	{
+		int totalprice=10;
+		System.out.println(totalprice);
+		boolean value=true;
+		char sample='A;
+		System.out.println(sample+"This is a char"+"boolean value is"+value);
+	}
+}
