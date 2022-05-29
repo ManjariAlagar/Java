9f811b272e7479c44d45597052fdf246df180eac
ManjariAlagar <56513108+ManjariAlagar@users.noreply.github.com>
2022-05-29 23:00:11 +0530

    Add files via upload

---
+++ Relationaloperators.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Relationaloperators{
+public static void main(String args[])
+{
+	Scanner sc=new Scanner(System.in);
+	int a,b;
+	a=sc.nextInt();
+	b=sc.nextInt();
+	System.out.println(a>=b);
+	System.out.println(a<=b);
+	char ch1='a';
+	char ch2='b';
+	System.out.println(ch1>=ch2);
+}
+}
