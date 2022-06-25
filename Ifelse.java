83492b9fc1e4b53b0a589e6dc2dfc0e494002a95
ManjariAlagar <56513108+ManjariAlagar@users.noreply.github.com>
2022-06-25 21:04:44 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Ifelse
+{
+public static void main(String args[])
+{
+int a=10;
+int b=20;
+int c=30;
+if(a>b && a>c)
+{
+System.out.println("a is the largest number");
+}
+else if(b>c && b>a)
+{
+System.out.println("b is the largest");
+}
+else{
+System.out.println("c is the largest");
+}
+}
+}
+
